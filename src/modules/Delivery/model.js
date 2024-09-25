a8dedf86f11012d8bf522735c9b81a0087e2997f
waseemAnsari11011 <waseemahm11011@gmail.com>
2024-09-25 11:42:08 +0530

    abc

---
+++ src/modules/Delivery/model.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Define the schema
+const deliverySchema = new Schema({
+  name: {
+    type: String,
+  },
+  image: {
+    type: String,
+  },
+  contactNumber: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    default: 'delivery man'
+  },
+  fcmDeviceToken: {
+    type: String,
+  },
+  isRestricted: {
+    type: Boolean,
+    default: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+
+// Create the model
+const Delivery = mongoose.model('Delivery', deliverySchema);
+
+module.exports = Delivery;
+//
